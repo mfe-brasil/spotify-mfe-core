99936b795e1e62ccf1b89d7060e2773ffe57ee6d
Eliseu dos Santos da Silva <s.eliseu@proton.me>
2025-04-21 21:18:49 -0300

    feat: create structure of spotify-mfe-core

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "mfe-brasil";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv?.isLocal,
+          orgName,
+        },
+      }),
+    ],
+  });
+};
